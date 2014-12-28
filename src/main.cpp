92e9b66bca83582e587090fd1647f61129a0566b
Shitaro <apprentice.physicist.shitaro@gmail.com>
2014-12-29 00:09:18 +0900

    add comment

--- src/main.cpp
+++ src/main.cpp
@@ -2,29 +2,37 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+
 static void error_callback(int error, const char* description)
 {
 	fputs(description, stderr);
 }
+
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, GL_TRUE);
 }
+
 int main(void)
 {
 	GLFWwindow* window;
 	glfwSetErrorCallback(error_callback);
+	// Initialize the library
 	if (!glfwInit())
 		exit(EXIT_FAILURE);
+	// Create a windowed mode window and its OpenGL context
 	window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
 	if (!window)
 	{
 		glfwTerminate();
 		exit(EXIT_FAILURE);
 	}
+	// Make the window's context current
 	glfwMakeContextCurrent(window);
+
 	glfwSetKeyCallback(window, key_callback);
+	// Loop until the user closes the window
 	while (!glfwWindowShouldClose(window))
 	{
 		float ratio;
@@ -47,7 +55,10 @@ int main(void)
 		glColor3f(0.f, 0.f, 1.f);
 		glVertex3f(0.f, 0.6f, 0.f);
 		glEnd();
+
+		// Swap front and back buffers
 		glfwSwapBuffers(window);
+		// Poll for and process events
 		glfwPollEvents();
 	}
 	glfwDestroyWindow(window);
